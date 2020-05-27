189ffc15046743d5f9d4d696413d79cf39c811cf
iljared98 <52998588+iljared98@users.noreply.github.com>
2020-05-27 01:42:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ from os import path
 from configparser import ConfigParser as cp # Used to grab the token and command prefix
 from discord.ext import commands
 
-
 config = cp()
 cfgpath = os.getcwd() + "//ignis.cfg"
 config.read(cfgpath)
@@ -20,22 +19,16 @@ prefix = config.get("DEFAULT", "prefix")
 
 client = commands.Bot(command_prefix=prefix, case_insensitive=True, owner_id='138452291085664256')
 
-# FIXME: Add try/except blocks to avoid problems with the commands
+@client.event
+async def on_ready():
+  now_ready = datetime.now()
+  dt_str_ready = now_ready.strftime("%m/%d/%Y %H:%M:%S")
+  print("Ignis Bot ::::: ONLINE\n\n{}".format(dt_str_ready))
 
-@client.command()
-async def load(ctx, extension):
-  client.load_extension(f'cogs.{extension}')
-
-@client.command()
-async def unload(ctx, extension):
-  client.unload_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-  if filename.endswith('.py'):
-    client.load_extension(f'cogs.{filename[:-3]}')
-
-
-
-
+# Loading all cog files;
+  for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+      client.load_extension(f'cogs.{filename[:-3]}')
+  await client.change_presence(activity=discord.Game(name='Valorant sucks'))
 
 client.run(token)
