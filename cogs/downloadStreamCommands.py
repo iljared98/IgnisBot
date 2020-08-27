c5df5d1fc5b53ddec754a71b3829668ac5edfa88
iljared98 <52998588+iljared98@users.noreply.github.com>
2020-08-27 05:12:02 -0500

    Update downloadStreamCommands.py

--- cogs/downloadStreamCommands.py
+++ cogs/downloadStreamCommands.py
@@ -16,28 +16,6 @@ class downloadStreamCommands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    # FIXME : Add global command cooldown of 30 seconds so multiple requests for the command
-    #       don't fuck it up! Or find a more elegant implementation for this process, whatever works!
-    #       Also, find a way to get the .zip file to the user.
-
-    # Use this to host files, maybe find a way to grab the .zip off of the page after uploading.
-    # https://volafile.org/r/19yk6apuc
-    '''
-    @commands.command(aliases=['chan','chandownload','chanarchive'])
-    async def chanscrape(self, ctx, *, input):
-        try:
-            chanpath = 'h'
-            if '4chan' or '4channel' in input:
-                await ctx.send(f'{ctx.mention.author} Downloading requested thread, please wait until the process is complete!')
-            elif '8kun.top' in input:
-                await ctx.send(f'{ctx.mention.author} Downloading requested thread, please wait until the process is complete!')
-            elif '9chan' in input:
-                await ctx.send(f'{ctx.mention.author} Downloading requested thread, please wait until the process is complete!')
-            else:
-                await ctx.send(f':warning: {ctx.mention.author}, the thread URL you gave is not valid. Please enter a valid thread URL and try again!')
-        except:
-            await ctx.send(f':warning: An error has occurred, please check your input and try again!')
-    '''
     @commands.command()
     async def ytInfo(ctx, URL, *args):
         with youtube_dl.YoutubeDL() as ydl:
