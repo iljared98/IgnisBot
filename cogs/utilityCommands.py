ee7d1525b04bf0b4d75190472c9df7592a2ce425
iljared98 <52998588+iljared98@users.noreply.github.com>
2020-12-20 02:23:15 -0600

    Update utilityCommands.py

--- cogs/utilityCommands.py
+++ cogs/utilityCommands.py
@@ -1,20 +1,33 @@
 import discord
 from discord.ext import commands
+import datetime
 from datetime import datetime
 import platform
+#import pandas as pd
+#from iexfinance.stocks import get_historical_data
+#from iexfinance.stocks import get_historical_intraday
+from iexfinance.stocks import Stock
 from wikipedia import wikipedia as wk
 import pyshorteners
 #import google
 import wolframalpha as wa
-
-import bs4
+from pyowm import OWM
+#import bs4
 import os
+import nasapy
+#import json
 from configparser import ConfigParser as cp
 
 config = cp()
 cfgpath = os.getcwd() + "//ignis.cfg"
 config.read(cfgpath)
+
 WA_KEY = config.get("API_KEYS", "wolfkey")
+STOCK_KEY = config.get("API_KEYS", "iexcloudPUBLISH")
+STOCK_KEY_PRIV = config.get("API_KEYS", "iexcloudSECRET")
+NASA_KEY = config.get("API_KEYS", "nasa")
+OWM_KEY = config.get("API_KEYS", "openweathermap")
+
 
 class Utility(commands.Cog):
 
@@ -33,12 +46,15 @@ class Utility(commands.Cog):
     dt_str = now.strftime("%m/%d/%Y %H:%M:%S")
     await ctx.send(f'Current Time and Date : {dt_str}')
 
-  @commands.command() # Number of guilds joined, users in current guild, python version, maybe add more?
+  @commands.command() # FIXME: Number of guilds joined, users in current guild, python version, maybe add more?
   async def stats(self, ctx):
     pyVersion = platform.python_version()
     serversJoined = len(self.bot.guilds)
     userCount = len(set(self.bot.get_all_members()))
-    statEmbed = discord.Embed(title="IgnisBot Statistics",description=f":snake: **Python Version:** {pyVersion}\n\n:computer: **Server Count:** {serversJoined}\n\n:family: **Users Count:** {userCount}")
+    statEmbed = discord.Embed(title="IgnisBot Statistics",
+                              description=f":snake: **Python Version:** {pyVersion}"
+                                          f"\n\n:computer: **Server Count:** {serversJoined}"
+                                          f"\n\n:family: **Users Count:** {userCount}")
 
     await ctx.send(embed=statEmbed)
 
@@ -52,6 +68,7 @@ class Utility(commands.Cog):
       embed.add_field(name='**Cogs**', value=cog_desc)
       await ctx.send(embed=embed)
 
+  #FIXME: Rewrite this entire command or find a module that does its job better.
   @commands.command(aliases=['wikipedia', 'wikisearch', 'wik', 'wikiquery','wk'])
   async def wiki(self, ctx, *args):
     try:
@@ -80,7 +97,7 @@ class Utility(commands.Cog):
     await ctx.send(embed=embed)
 
   @commands.command(aliases=['wolfram','wa','wolf'])
-  async def wolframalpha(self, ctx, *, query):
+  async def wolframalpha(self, ctx, *, query): #FIXME: Add support for answers to return images; such as graphs, diagrams etc...
     try:
       client = wa.Client(WA_KEY)
       embed = discord.Embed(description=f'**__Question:__** *{query}*')
@@ -94,6 +111,63 @@ class Utility(commands.Cog):
       await ctx.send(embed=embed)
     except:
       await ctx.send(f':warning: {ctx.author.mention} Wolfram|Alpha was not able to process your query. Please try again.')
+
+  @commands.command(name='stock', aliases=['stocks'])
+  async def stockCommand(self, ctx, ticker):
+    start = datetime.now() #FIXME: Potentially use another API instead, none of the desired fields will return despite
+                           # being used according to the directions of their documentation. Troubleshoot this ASAP
+
+    stock = Stock(str(ticker), token=STOCK_KEY)
+
+    #df = get_historical_intraday(ticker, output_format='pandas', token=STOCK_KEY)
+    #df = df.reindex(index=df.index[::-1])
+    embed = discord.Embed()
+    embed.set_author(name=stock.get_company_name())
+    embed.add_field(name="Stock Price", value=f'${stock.get_price()}')
+    #embed.add_field(name="Sector", value=stock.get_sector())
+    #embed.add_field(name="Year High", value=f'${stock.get_years_high()}')
+    #embed.add_field(name="Year Low", value=f"${stock.get_years_low()}")
+
+    await ctx.send(embed=embed)
+
+  @commands.command(name='potd')
+  async def potdCommand(self, ctx):
+    nasa = nasapy.Nasa(key=NASA_KEY)
+    d = datetime.today().strftime('%Y-%m-%d')
+    apod = nasa.picture_of_the_day(date=d, hd=True)
+    try:
+
+      embed = discord.Embed(title=f'{apod["title"]}',
+                            description=f'{apod["explanation"]}')
+      embed.set_image(url=f'{apod["hdurl"]}')
+      embed.set_footer(text=f'NASA Picture of the Day : {datetime.today().strftime("%m-%d-%Y")}')
+      await ctx.send(embed=embed)
+    except: #FIXME: In case the description exceeds Discord's limits, it will simply send the embed without it. Be sure to clean up
+            # this error checking in case it causes other problems.
+      embed = discord.Embed(title=f'{apod["title"]}')
+      embed.set_image(url=f'{apod["hdurl"]}')
+      embed.set_footer(text=f'NASA Picture of the Day : {datetime.today().strftime("%m-%d-%Y")}')
+      await ctx.send(embed=embed)
+
+
+  @commands.command(name='weather') # FIXME: Add more specific functionality later, for now just enable city searching.
+  async def weatherCommand(self, ctx, query):
+    try:
+      owm = OWM(OWM_KEY)
+      mgr = owm.weather_manager()
+
+      observation = mgr.weather_at_place(query)
+      w = observation.weather
+
+      embed = discord.Embed(title=f'{query.capitalize()}')
+      embed.add_field(name='Temp Low', value=f'{w.temperature("fahrenheit")["temp_min"]} °F')
+      embed.add_field(name='Temp High', value=f'{w.temperature("fahrenheit")["temp_max"]} °F')
+      embed.add_field(name='Humidity', value=f'{w.humidity}%')
+      embed.add_field(name='Wind Speed', value=f'{round(float(w.wind(unit="miles_hour")["speed"]), 2)} mph')
+
+      await ctx.send(embed=embed)
+    except:
+      await ctx.send(f':warning: {ctx.author.mention} There was an issue with your Openweathermap query. Please try again.')
 
 def setup(bot):
   bot.add_cog(Utility(bot))
